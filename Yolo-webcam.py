e60d21a5c167ef73566867b1fb59dccd37a8b030
Rishika Gupta <104383360+Rishika24g@users.noreply.github.com>
2024-09-20 19:48:17 +0530

    Add files via upload

---
+++ Yolo-webcam.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+
+cap=cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+model = YOLO("../Yolo-Weights/yolov8n.pt")
+
+while True:
+    success, img =cap.read()
+    results=model(img, stream=True)
+    for r in results:
+        boxes =r.boxes
+        for box in boxes:
+            x1,y1,x2,y2= box.xyxy[0]
+            x1, y1, x2, y2 = int(x1),y1,x2,y2
+            print(x1,y1,x2,y2)
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+
